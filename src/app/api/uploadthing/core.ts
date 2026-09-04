92f66758e65b29fcf5063aeb8cda724a5090dcc4
cleanthing6-jpg <katiewalter1999@gmail.com>
2026-09-04 02:11:47 +0100

    Add UploadThing audio/image uploads

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,16 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  audioUploader: f({ audio: { maxFileSize: "64MB", maxFileCount: 1 } })
+    .onUploadComplete(async ({ file }) => {
+      return { url: file.ufsUrl };
+    }),
+  imageUploader: f({ image: { maxFileSize: "8MB", maxFileCount: 3 } })
+    .onUploadComplete(async ({ file }) => {
+      return { url: file.ufsUrl };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
